8c2578a2602a47d612e4c2def76e9a33d34fa397
Loïc Gasser <loicgasser4@gmail.com>
2018-08-28 20:30:45 -0400

    Remove duplicated selector

--- src/actions/filters.js
+++ src/actions/filters.js
@@ -44,20 +44,3 @@ export const setEndDate = (endDate) => (
     }
 
 )
-
-// Combined reducers
-
-const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
-    return expenses.filter((expense) => {
-        const textMatch = expense.description.toLowerCase().includes(text.toLowerCase())
-        const startDateMatch = typeof startDate !== 'number' ||  expense.createdAt >= startDate
-        const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate
-        return textMatch && startDateMatch && endDateMatch
-    }).sort((a, b) => {
-        if (sortBy === 'date') {
-            return a.createdAt > b.createdAt ? 1 : -1
-        } else if (sortBy == 'amount') {
-            return a.amount > b.amount ? 1 : -1
-        }
-    })
-}
