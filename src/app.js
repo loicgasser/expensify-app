e49035a03053382803c6d4f48b5e8e0d78fee05b
loicgasser <loicgasser4@gmail.com>
2018-09-07 14:03:12 -0400

    Setup prod build

--- src/app.js
+++ src/app.js
@@ -5,10 +5,10 @@ import { Provider } from 'react-redux'
 import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore'
 import { addExpense } from './actions/expenses'
+import moment from 'moment'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 import 'react-dates/lib/css/_datepicker.css'
-import moment from 'moment'
 
 const store = configureStore()
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,65 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
+module.exports = (env) => {
+    const isProduction = env == 'production'
 
-module.exports = {
-    mode: 'development',
-    entry: './src/app.js',
-    watch: true,
-    watchOptions: {
-        poll: true,
-        ignored: /node_modules/
-    },
-    output: {
-        path: path.join(__dirname, '/public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                test: /\.js$/
-            },
-            {
-                test: /\.s?css$/,
-                use: [
-                    "style-loader",
-                    "css-loader",
-                    "sass-loader"
-                ]
-            }
-        ]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, '/public'),
-        compress: false,
-        port: 9000,
-        historyApiFallback: true
+    return {
+        mode: 'development',
+        entry: './src/app.js',
+        watch: isProduction ? false : true,
+        watchOptions: {
+            poll: true,
+            ignored: /node_modules/
+        },
+        output: {
+            path: path.join(__dirname, '/public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [
+                {
+                    loader: 'babel-loader',
+                    exclude: /node_modules/,
+                    test: /\.js$/
+                },
+                {
+                    test: /\.s?css$/,
+                    use: [
+                        isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: 'styles.css',
+                chunkFilename: 'styles.[hash].css'
+            }),
+            new HtmlWebpackPlugin({
+                template: 'public/index.template.html',
+                minify: true
+            })
+        ],
+        devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, '/public'),
+            compress: false,
+            port: 9000,
+            historyApiFallback: true
+        }
     }
 }
